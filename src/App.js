5c975494f93356c65efdf1c6a284b74ce8dd1f00
github username <github email address>
2018-08-15 00:17:32 -0400

    test

--- src/App.js
+++ src/App.js
@@ -2,35 +2,59 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import hex from './mpvl.svg';
 import hexMini from './mpvl-mini.svg';
+import octopus from './octopus-01.svg'
 import './App.css';
 
+
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      terms: ["Dogs","People", "Games", "Beer",]
+    }
+  }
+
   render() {
     return (
       <div className="App">
+
         <div className="orb">
         </div>
         <div className="orb2">
         </div>
-        <img src={hex} className="hex-logo"/>
-        <img src={hexMini} className="hex-logo-orbit"/>
+        <div className="flex-container">
+          <div className="logo-flex-container">
+            <img src={hex} className="hex-logo"/>
+            <img src={hexMini} className="hex-logo-orbit"/>
+          </div>
 
-        <div className="hex-logo">
-        </div>
+          <div className="nav-flex-container">
+            <div className="about-menu">
+              about
+            </div>
+            <div className="projects-menu">
+              projects
+            </div>
+            <div className="contact-menu">
+              contact
+            </div>
+          </div>
 
-        <div className="title-fade-in">
-          matthew lawford
-        </div>
-        <div className="about-menu">
-          about
-        </div>
+          <div className="flex-title-container">
+            <div className="title-fade-in">
+              Hi,
+            </div>
+            <div className="title-fade-in-2">
+              My name is Matt
+            </div>
+            <div className="title-fade-in-3">
+            I like making things
+            </div>
 
-        <div className="projects-menu">
-          projects
-        </div>
+          </div>
+          <div className="gradient-bar">
+          </div>
 
-        <div className="contact-menu">
-          contact
         </div>
       </div>
     );
---
+++ src/project.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import hex from './mpvl.svg';
+import hexMini from './mpvl-mini.svg';
+import octopus from './octopus-01.svg'
+import './App.css';
+
+
+class App extends Component {
+  constructor(){
+  }
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
